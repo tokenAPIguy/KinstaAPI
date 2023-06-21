001119d4be3934ca66f3f9ef75ffbc3a92833a7d
levyjake808@gmail.com <levyjake808@gmail.com>
2023-06-21 14:24:37 -0400

    Added Clear Cache + Restart PHP

--- fetchData.js
+++ fetchData.js
@@ -1,18 +1,22 @@
+const token =
+  "3c4d168fa1725fe833d239c5221510445f4b8a5ffbc6cf06cdc742816886c5f7";
+
 // Fetch Site List
 async function fetchSiteList(companyId) {
-  const url = `https://api.kinsta.com/v2/sites?company=${companyId}`;
-  const token =
-    "3c4d168fa1725fe833d239c5221510445f4b8a5ffbc6cf06cdc742816886c5f7";
+  const res = await fetch(
+    `https://api.kinsta.com/v2/sites?company=${companyId}`,
 
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
 
   try {
-    const res = await axios.get(url, config);
-    console.log(res.data);
+    const data = await res.json();
+    console.log(data);
   } catch (error) {
     console.error(error);
   }
@@ -20,19 +24,62 @@ async function fetchSiteList(companyId) {
 
 // Fetch Site by ID
 async function fetchSiteByID(siteId) {
-  const url = `https://api.kinsta.com/v2/sites/${siteId}`;
-  const token =
-    "3c4d168fa1725fe833d239c5221510445f4b8a5ffbc6cf06cdc742816886c5f7";
+  const res = await fetch(
+    `https://api.kinsta.com/v2/sites/${siteId}`,
 
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
 
   try {
-    const res = await axios.get(url, config);
-    console.log(res.data);
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Clear Kinsta Cache
+async function clearKinstaCache(envId) {
+  const res = await fetch(`https://api.kinsta.com/v2/sites/tools/clear-cache`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      environment_id: `${envId}`,
+    }),
+  });
+
+  try {
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Restart PHP
+async function restartPHP(envId) {
+  const res = await fetch(`https://api.kinsta.com/v2/sites/tools/restart-php`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      environment_id: `${envId}`,
+    }),
+  });
+
+  try {
+    const data = await res.json();
+    console.log(data);
   } catch (error) {
     console.error(error);
   }
--- main.js
+++ main.js
@@ -1,17 +1,37 @@
-// Retrieve DOM elements
+// Fetch Site List by Company ID
 const companyIdInput = document.getElementById("companyID");
-const companyIdButton = document.getElementById("fetchCompanyID");
+const companyIdButton = document.getElementById("fetchCompany");
 
-const siteIdInput = document.getElementById("siteID");
-const siteIdButton = document.getElementById("fetchSiteID");
-
-// Creating Event listeners
 companyIdButton.addEventListener("click", () => {
   const companyId = companyIdInput.value;
   fetchSiteList(companyId);
 });
 
+// Fetch Site by ID
+const siteIdInput = document.getElementById("siteID");
+const siteIdButton = document.getElementById("fetchSite");
+
 siteIdButton.addEventListener("click", () => {
   const siteId = siteIdInput.value;
   fetchSiteByID(siteId);
+});
+
+// Clear Cache
+const clearCacheInput = document.getElementById("clearCacheInput");
+const clearCacheButton = document.getElementById("clearCache");
+
+clearCacheButton.addEventListener("click", () => {
+  const envId = clearCacheInput.value;
+  console.log(envId);
+  clearKinstaCache(envId);
+});
+
+// Restart PHP
+const restartPHPInput = document.getElementById("restartPHPInput");
+const restartPHPButton = document.getElementById("restartPHP");
+
+restartPHPButton.addEventListener("click", () => {
+  const envId = restartPHPInput.value;
+  console.log(envId);
+  restartPHP(envId);
 });
